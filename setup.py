b5bae188601536c88ec07c06f397837744d968a6
Monty Dimkpa <cmdimkpa@gmail.com>
2020-09-19 13:42:51 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'tdmf',
   packages = ['tdmf'],
-  version = '0.5.7',
+  version = '0.5.8',
   license='MIT',
   description = 'A Test-Driven Modular Application Framework for Python',
   author = 'Monty Dimkpa',
   author_email = 'cmdimkpa@gmail.com',
   url = 'https://github.com/cmdimkpa/tdmf',
-  download_url = 'https://github.com/cmdimkpa/tdmf/archive/v_05_07.tar.gz',
+  download_url = 'https://github.com/cmdimkpa/tdmf/archive/v_05_08.tar.gz',
   keywords = ['unit tests', 'package tests', 'atomic functions', 'pipelines', 'workflows', 'mutable state'],
   install_requires=[            
           
