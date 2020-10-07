3f58f185fff1e125249229100e3e22e81325ce0c
Monty Dimkpa <cmdimkpa@gmail.com>
2020-10-07 09:59:13 +0100

    Add files via upload

---
+++ examples/js/recipe-scraper/app.js
@@ -0,0 +1,29 @@
+// process code: recipe scraper
+
+tdmf = require('tdmf')
+require('./components.js')
+
+tdmf.testEngine.add_test('package', 'fetch_page_content', 'pt_string_only')
+tdmf.testEngine.add_test('unit', 'fetch_page_content', ['fetch-test', ['https://cmdimkpa.github.io/test.txt'], ['test\n'] ])
+
+// pipeline to farm recipe links from entry page
+farm_recipe_links = tdmf.Pipeline([
+    [ "fetch_page_content", ["https://www.foodhero.org/recipes/categories/42"] ],
+    "parse_recipe_links"
+])
+
+// pipeline to extract JSON recipes from recipe links
+extract_JSON_recipes = tdmf.Pipeline([
+    [ "get_JSON_recipes", "farm_recipe_links" ]
+])
+
+// workflow to manage process flow
+recipe_workflow = tdmf.Workflow([
+    "farm_recipe_links",
+    "extract_JSON_recipes"
+])
+
+// run workflow
+tdmf.run_workflow( recipe_workflow ).then(( workflow ) => {
+        console.log( workflow.output )
+})
